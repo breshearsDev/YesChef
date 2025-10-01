8832be532a3dd29dfb33437227818f69c4f1eca9
Rob Breshears <robbreshears@gmail.com>
2025-09-30 22:17:20 -0600

    Initial Commit

---
+++ YesChef/YesChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesChefApp.swift
+//  YesChef
+//
+//  Created by Breshears, Rob - CTC on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct YesChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YesChefTests/YesChefTests.swift
@@ -0,0 +1,17 @@
+//
+//  YesChefTests.swift
+//  YesChefTests
+//
+//  Created by Breshears, Rob - CTC on 9/30/25.
+//
+
+import Testing
+@testable import YesChef
+
+struct YesChefTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
