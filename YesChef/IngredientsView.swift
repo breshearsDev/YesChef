618330a4bf5bf5a0705bd2c4ef2e18dacafc6669
Rob Breshears <robbreshears@gmail.com>
2025-10-02 15:36:55 -0600

    Animation and Double feature finished.

--- YesChef/IngredientsView.swift
+++ YesChef/IngredientsView.swift
@@ -15,15 +15,16 @@ struct IngredientsView: View {
     @State private var magic = 6
     @State private var frosting = 8
     @State private var count = 2
+    @State private var animate = false
     
     var body: some View {
         VStack {
-            
+            Spacer()
             HStack {
                 Image(systemName: "birthday.cake")
                     .font(.system(size: 60))
                     .foregroundStyle(.white)
-
+                
                 Text("Ingredients")
                     .font(.system(size: 30))
                     .foregroundStyle(.white)
@@ -42,7 +43,7 @@ struct IngredientsView: View {
             .fontWeight(.semibold)
             .font(.title2)
             .fontDesign(.rounded)
-            
+            Spacer()
             HStack {
                 Button("x1") {
                     flour = 2
@@ -51,24 +52,36 @@ struct IngredientsView: View {
                     magic = 6
                     frosting = 8
                     count = 2
+                    animate.toggle()
                 }
                 .buttonStyle(.borderedProminent)
                 
                 Button("x2") {
                     double(multipler: 2)
+                    animate.toggle()
                 }
                 .buttonStyle(.borderedProminent)
                 .disabled(count > 2)
                 
                 Button("x 3") {
                     double(multipler: 3)
+                    animate.toggle()
                 }
                 .buttonStyle(.borderedProminent)
                 .disabled(count > 3)
             }
+            Image(systemName: "wand.and.outline")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .foregroundStyle(animate ? .white : .green)
+                .animation(.default, value: animate)
+            
+            
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(LinearGradient(colors: [.pink, .white], startPoint: .top, endPoint: .bottom))
+        
     }
     
     func double(multipler: Int) {
@@ -78,6 +91,7 @@ struct IngredientsView: View {
         magic *= multipler
         frosting *= multipler
         count *= multipler
+        
         
         
     }
